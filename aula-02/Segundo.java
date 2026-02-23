6137ed813cf31307bb612e582ce1fabe072fa3e7
Hiago Ritter Santos <hiago.s2006@aluno.ifsc.edu.br>
2026-02-23 09:19:21 -0300

    Começando com a linguagem Java

---
+++ aula-02/Segundo.java
@@ -0,0 +1,45 @@
+void main() {
+
+    // int i = 10;
+    // String resultado = "iniciando";
+
+    // switch (i) {
+    //     case 1:
+    //         IO.println("1");
+    //         break;
+    //     case 2:
+    //         IO.println("2");
+    //         break;
+    //     case 3:
+    //         IO.println("3");
+    //         break;
+    //     default:
+    //         resultado = "Outro valor...";
+    // }
+
+
+    // String resultado_dois = switch(i) {
+    //     case 1 -> "Um";
+    //     case 2 -> "Dois";
+    //     case 3 -> "Três";
+    //     default -> "Outro valor";
+    // }
+
+    // IO.println(resultado);
+
+    // exercicio em sala
+    int valor = 1;
+
+    int[][] matriz = new int [3][3];
+    for(int i=0; i<3; i++) {
+        for (int j=0; j<3; j++){
+            matriz[i][j] = valor;
+
+            if (i == j) {
+                IO.print(matriz[i][j]);
+            }
+
+            valor++;
+        }
+    }
+}
