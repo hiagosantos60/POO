e651886802619c2fac72c8f50684158607cf30cb
Hiago Ritter Santos <hiago.s2006@aluno.ifsc.edu.br>
2026-02-23 08:31:36 -0300

    Primeiro olá mundo em Java

---
+++ aula-02/OlaMundo.java
@@ -0,0 +1,8 @@
+// Em java utilizamos o CamelCase para escrever o codigo
+
+public class OlaMundo {
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!");
+    }
+}
+
