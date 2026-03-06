4b2a6e537e1da19f4ed1663d715b04d714462a7d
Hiago Santos <hiagorsantos60@gmail.com>
2026-03-05 22:35:16 -0300

    lab02 ex03

--- lab02/ex03/app/src/main/java/lab02/ex03/App.java
+++ lab02/ex03/app/src/main/java/lab02/ex03/App.java
@@ -3,11 +3,14 @@ package lab02.ex03;
 public class App {
     public static void main(String[] args) {
         
-        String tipo = "triangulo"; // para o tipo de figura que meu usuário irá passar 
+        if (args.length < 2) {
+            IO.println("Entreda invalida... ");
+        }
+
+        String tipo = args[0];
         tipo = tipo.toLowerCase();
 
-        int tam = Integer.parseInt(args[1]);
-        int alt = Integer.parseInt(args[2]);       
+        int tam = Integer.parseInt(args[1]);      
 
         if (tam < 2) {
             IO.println("Entrada invalida! ");
@@ -24,15 +27,44 @@ public class App {
         }
 
         if (tipo.equals("losango")) {
-            if (tam%2 == 0){ // evita entrada de numeros pares
-                IO.println("Impossivel represetar nesse tamanho... ");
+            if (tam % 2 == 0) {
+                IO.println("Impossivel representar nesse tamanho... ");
                 return;
             }
 
-            int meio = Math.round(tam/2);
+            int meio = tam / 2;
+            int c = 0;
 
-            
+            while (c < tam) {
+                int esp = Math.abs(meio - c);
+                int ast = tam - (2 * esp);
+        
+                IO.println(" ".repeat(esp) + "*".repeat(ast));
+        
+                c++;
+            }
+        }
 
+        if (tipo.equals("retangulo")) {
+            if (args.length < 3) {
+                IO.print("Entrada inválida para retangulo");
+            }
+
+            int alt = Integer.parseInt(args[2]); 
+
+            int altura = alt;
+
+            for (int i = alt; i >= 0; i--) {
+                if (i == 0 || i == altura) {
+                    IO.print("*".repeat(tam));
+                    IO.println();
+                } else {
+                    IO.print("*");
+                    IO.print(" ".repeat(tam-2));
+                    IO.print("*");
+                    IO.println();
+                }
+            }
         }
 
     }
