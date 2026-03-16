9652412fdd7f55a6089f73d55914e4cd41c8aab7
Hiago Ritter Santos <hiago.s2006@aluno.ifsc.edu.br>
2026-03-16 09:11:26 -0300

    Ex01 Aula08

--- ex-aula08/ex-contador/app/src/main/java/engtelecom/poo/App.java
+++ ex-aula08/ex-contador/app/src/main/java/engtelecom/poo/App.java
@@ -2,10 +2,15 @@ package engtelecom.poo;
 
 public class App {
     public static void main(String[] args) {
+    
+        //Exemplos de uso para o Contador
         Contador c = new Contador();
         c.setValor(50);
         IO.println(c.getValor());
         c.incrementar();
         IO.println(c.getValor());
-    }
+
+
+
+        }
 }
