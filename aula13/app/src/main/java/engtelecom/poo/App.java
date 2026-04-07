ef075e77098340a510ad920402cfec76757e49d9
Hiago Ritter Santos <hiago.s2006@intelbras.com.br>
2026-04-07 18:47:34 -0300

    Encerrando livraria aula 13

--- aula13/app/src/main/java/engtelecom/poo/App.java
+++ aula13/app/src/main/java/engtelecom/poo/App.java
@@ -34,10 +34,15 @@ public class App {
                 consultarPorISBN(iSBN);
                 break;
             case 4:
+                autor = IO.readln("Qual o nome do autor: ").toLowerCase();
+                consultarPorNome(autor);
                 break;
             case 5:
+                atualizarLivro();
                 break;
             case 6:
+                String ISBN = IO.readln("Qual o ISBN do livro que deseja remover: ");
+                removerPorISBN(ISBN);
                 break;
             default:
                 System.out.println("Opção inválida!");
@@ -64,6 +69,49 @@ public class App {
         }
     }
 
+    private void consultarPorNome(String autor) {
+        for (Livro livro : acervo.values()) {
+            if(livro.getAutor().toLowerCase().contains(autor)){
+                System.out.println(livro);
+            }
+        }
+    }
+
+    private void atualizarLivro() {
+        String isbn = IO.readln("Qual o ISBN que deseja atualizar: ");
+        System.out.println("Menu de atualização");
+        System.out.println("1 - Titulo:");
+        System.out.println("2 - Autor");
+        System.out.println("3 - Ano de publicação");
+        int opcao = Integer.parseInt(IO.readln("O que deseja atualizar: "));
+
+        Livro livro = acervo.get(isbn);
+
+        switch (opcao) {
+        case 1:
+            String novoTitulo = IO.readln("Qual o novo título: ");
+            livro.setTitulo(novoTitulo);
+            break;
+        case 2:
+            String novoAutor = IO.readln("Qual o novo autor: ");
+            livro.setAutor(novoAutor);
+            break;
+        case 3:
+            int novoAno = Integer.parseInt(IO.readln("Qual o novo ano: "));
+            livro.setAno(novoAno); // Use o setAno e converta para int
+            break;
+        default:
+            System.out.println("Opção inválida.");
+            break;
+    }
+    }
+
+    public void removerPorISBN(String ISBN){
+        if(acervo.containsKey(ISBN)) {
+            acervo.remove(ISBN);
+        }
+    }
+
     public static void main(String[] args) {
 
         /*
@@ -81,15 +129,10 @@ public class App {
 
         while (opcao!=7) {
             app.exibirMenu();
-
+            opcao = Integer.parseInt(IO.readln("O que deseja fazer: "));
             if(opcao != 7){
-                opcao = Integer.parseInt(IO.readln("O que deseja fazer: "));
                 app.executarMenu(opcao);
-            } else {
-                break;
-            }
+            } 
         }
-
-
     }
 }
