6f0107c5bca848eed71ccc6dd852af50b445bb7e
Hiago Ritter Santos <hiago.s2006@aluno.ifsc.edu.br>
2026-05-06 10:24:03 -0300

    Adicionando biblioteca nas configurações do gradle

---
+++ lab06/app/src/main/java/poo/engetelecom/App.java
@@ -0,0 +1,13 @@
+package poo.engetelecom;
+
+import edu.princeton.cs.algs4.Draw;
+
+public class App {
+    public static void main(String[] args) {
+
+        Draw draw = new Draw();
+
+        
+
+    }
+}
